3616ddba8868ff22e959814381fbc4abf4ad4dce
Vijay Kumawat <vijaykumawat@Vijays-MacBook-Air.local>
2023-02-08 18:26:00 +0530

    updated code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-$('.owl-carousel').owlCarousel({
+$('.common-slider').owlCarousel({
   loop:true,
   margin:10,
   nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+  items:1,
+  dots: false,
+})
+$('.upcoming-movies-slider').owlCarousel({
+  loop:true,
+  margin:10,
+  items: 1,
+  nav:false,
+  dots: true,
+  animateOut: 'slideOutUp',
+  animateIn: 'slideInUp',
+  smartSpeed:450,
+  mouseDrag:false
+
 })
